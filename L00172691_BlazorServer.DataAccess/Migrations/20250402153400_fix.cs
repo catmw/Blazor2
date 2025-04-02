9ea9b237e1a8c396506933c307980ed910c2231e
Catherine Whiteside <157846537+catmw@users.noreply.github.com>
2025-04-02 17:01:52 +0100

    start

---
+++ L00172691_BlazorServer.DataAccess/Migrations/20250402153400_fix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace L00172691_BlazorServer.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
